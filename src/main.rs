d178e0fba9984911b742f76973bd3ba95f560d59
Alex Newman <posi@planet.com>
2015-12-21 17:47:40 -0800

    Link postgresql

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+extern crate libc;
+
+#[link(name = "pgcommon")]
+extern "C" {
+    pub fn pg_str_endswith(s1: *const ::libc::c_char,
+                           s2: *const ::libc::c_char) -> ::libc::c_int;
+}
+
+fn main() {
+    let a = std::ffi::CString::new("a").unwrap();
+    let b = std::ffi::CString::new("b").unwrap();
+    unsafe {
+        println!("A ends with A: {}", pg_str_endswith(a.as_ptr(), a.as_ptr()));
+        println!("A ends with B: {}", pg_str_endswith(a.as_ptr(), b.as_ptr()));
+    }
+}
+
+
